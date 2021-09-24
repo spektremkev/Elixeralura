cb75a8380b1359bfb629fff3d3b20b886ebfa99b
Gabriel Dos S N Martins <50385858+spektremkev@users.noreply.github.com>
2021-09-24 18:14:39 -0300

    Alula_02

--- Teste.exs
+++ Teste.exs
@@ -2,8 +2,14 @@ defmodule MeuModulo do
   import IO, only: [puts]
   import Kernel, except: [inspect: 1]
 
+  alias MeuModulo.Math, as: MyMath
+
   def ola_mundo  do
-    inspect ("Olá Mundo")
+    inspect (Math.soma(1, 2))
+  end
+
+  def exibe_se_eh_par(numero) do
+    puts ("O número #{numero} é par? #{Integer.is_even(numero)}" )
   end
 
   def inspect(parametro)  do
@@ -11,4 +17,5 @@ defmodule MeuModulo do
     puts (parametro)
     puts ("Fim da inspeção")
   end
+
 end
