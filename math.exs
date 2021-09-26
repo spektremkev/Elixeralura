73e3bc8caf9a67c41797bdd37296221565e27128
Gabriel Dos S N Martins <50385858+spektremkev@users.noreply.github.com>
2021-09-26 12:05:02 -0300

    Recursão, Enums
    Adição feita no arquivo math

--- math.exs
+++ math.exs
@@ -5,4 +5,7 @@ defmodule MeuModulo.Math do
   #defmodule MeuModulo.Math do
   #   def soma(parametro1, parametro2), do: parametro1 + parametro2
   #end
+    def zero? (0), do: true
+    def zero? (x) when (is_integer(x)), do: false
+
 end
